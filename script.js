e817e0737e3e4612b4ffd69b4b81db82391a1c44
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-29 01:12:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  if (typeof name !== "string" || typeof age !== "number") {
+    return;
+  }
+  this.name = name;
+  this.age = age;
+  this.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  };
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+  this.jobGreet = function () {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  };
+}
+const person = new Person("Alice", 25);
+person.greet();
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
